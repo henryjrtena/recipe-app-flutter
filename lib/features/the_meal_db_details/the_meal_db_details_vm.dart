292b22b5faa58b4223fa6ae42c085c37b5172d5a
henryjrtena <henrytena2013@gmail.com>
2023-04-12 14:05:03 +0800

    feat: delete recipe action - modified the mmvm for the details page - show a snackbar with delete and cancel if the user wishes to delete the recipe - modified the popupmenu button to accomodate the delete recipe feature - added the action for the delete recipe - fluuter analyzed and dart formatted

--- lib/features/the_meal_db_details/the_meal_db_details_vm.dart
+++ lib/features/the_meal_db_details/the_meal_db_details_vm.dart
@@ -3,25 +3,31 @@ import 'package:recipe_app_flutter/features/the_meal_db_details/the_meal_db_deta
 import 'package:recipe_app_flutter/state/action/recipe_actions.dart';
 import 'package:recipe_app_flutter/state/app_state.dart';
 import 'package:async_redux/async_redux.dart';
+import 'package:flutter/material.dart';
 
 class TheMealDBApiDetailsVmFactory extends VmFactory<AppState, TheMealDBApiDetailsConnector> {
   @override
   Vm fromStore() => TheMealDBApiDetailsVm(
         recipeDetails: _recipeDetails,
         onUpdateRecipeNote: _onUpdateRecipeNote,
+        onDeleteRecipe: _onDeleteRecipe,
       );
 
   Recipe get _recipeDetails => state.recipeDetails!;
 
   void _onUpdateRecipeNote(String note) => dispatchSync(OnUpdateRecipeNotesAction(recipeNote: note));
+
+  void _onDeleteRecipe() => dispatchSync(OnDeleteRecipeAction());
 }
 
 class TheMealDBApiDetailsVm extends Vm {
   TheMealDBApiDetailsVm({
     required this.recipeDetails,
     required this.onUpdateRecipeNote,
+    required this.onDeleteRecipe,
   }) : super(equals: [recipeDetails]);
 
   final Recipe recipeDetails;
   final Function(String) onUpdateRecipeNote;
+  final VoidCallback onDeleteRecipe;
 }
